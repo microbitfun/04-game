61c63699a81c7970623c74c3955cce8e16f9c771
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-12 10:27:40 +0200

    updated code with book screenshot

--- main.ts
+++ main.ts
@@ -2,11 +2,6 @@ input.onButtonPressed(Button.A, function () {
     led.plot(randint(0, 4), randint(0, 4))
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    game.startCountdown(10000)
+    basic.clearScreen()
 })
